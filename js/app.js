4f90ed216e77dd7838e3ab53a3a8f54c7cc42cbf
Artem <artiom.ygryumov@gmail.com>
2021-11-06 13:41:57 +0300

    маска для поля телефона

--- js/app.js
+++ js/app.js
@@ -88,7 +88,63 @@ function accordion() {
 		window.scrollTo(0, distanceTop)
 	}
 }
-const burger = document.querySelector('.burger');
-burger.addEventListener('click', function(e) {
-  burger.classList.toggle('_active')
-})
+
+// Слайдер в разделах отзывах на экранах меньше 480px
+if (window.innerWidth <= 480) {
+	const swiper = new Swiper('.reviews__slider', {	
+		slidesPerView: 1, // Кол-во показываемых слайдов
+		spaceBetween: 0, // Расстояние между слайдами
+		// loop: true, // Бесконечный слайдер
+		// freeMode: true, // Слайдеры не зафиксированны
+
+		breakpoints: {
+			1200: {
+
+			},
+			700: {
+
+			},
+			400: {
+
+			}
+		},
+
+		navigation: {
+			nextEl: '.swiper__arrow-next',
+			prevEl: '.swiper__arrow-prev',
+		}
+	});
+}
+
+// Маска для телефона
+[].forEach.call( document.querySelectorAll('[type="tel"]'), function(input) {
+    var keyCode;
+    function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        var pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        var matrix = "+7 (___) ___ ____",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            new_value = matrix.replace(/[_\d]/g, function(a) {
+                return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+            });
+        i = new_value.indexOf("_");
+        if (i != -1) {
+            i < 5 && (i = 3);
+            new_value = new_value.slice(0, i)
+        }
+        var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+            function(a) {
+                return "\\d{1," + a.length + "}"
+            }).replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+        if (event.type == "blur" && this.value.length < 5)  this.value = ""
+    }
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false)
+});
